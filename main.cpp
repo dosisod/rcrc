1a89d800b01a5b0e51fc685e0b12940607e7df29
dosisod <39638017+dosisod@users.noreply.github.com>
2019-12-23 12:17:56 -0800

    added pretty printer for displaying data

--- main.cpp
+++ main.cpp
@@ -3,12 +3,31 @@
 #include "fileIO.cpp"
 #include "algs.cpp"
 
+void print(std::string);
+
 int main() {
-	File f("english.txt");
+	File f("testing.txt");
 
 	f.for_each([](std::string str) {
-		std::cout << str << "\n";
+		print(str);
 	});
 
 	return 0;
+}
+
+void print(std::string str) {
+	static int linenumber=2;
+	static std::string last;
+
+	if (last.length()>0) {
+		int repeated=first_non_match(str, last);
+		std::cout << repeated << "\t";
+		std::cout << std::string(repeated, 'x');
+		std::cout << "\t\t" << str << "\n";
+		linenumber++;
+	}
+	else {
+		std::cout << "\t\t\t" << str << "\n";
+	}
+	last=str;
 }
